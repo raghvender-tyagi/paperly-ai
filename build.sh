1ea9331ba7a437b5925650c4e073ba3ec756c359
raghvender-tyagi <sunilkumartyagi900@gmail.com>
2026-09-01 12:41:30 +0530

    fix: auto-trigger database migrations on startup and add build.sh for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
-# Create staticfiles directory if it doesn't exist
-mkdir -p staticfiles
-
 python manage.py collectstatic --no-input
 python manage.py migrate
--- paperlydjango/wsgi.py
+++ paperlydjango/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paperlydjango.settings')
 
 application = get_wsgi_application()
+
+# Run database migrations automatically on startup
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Startup migration warning: {e}")
+
